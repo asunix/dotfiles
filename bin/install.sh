5d87d5f034a5985de81898535b0dab5217720508
Giovanni Moroni <asunix@gmail.com>
2023-01-27 16:41:56 +0100

    Added a lot of function for the first installation of a Mac laptop.

--- bin/install.sh
+++ bin/install.sh
@@ -77,6 +77,7 @@ install_homebrew() {
 		# shellcheck source=/dev/null
 		source "$HOME"/.profile
 
+		export HOMEBREW_NO_INSTALL_CLEANUP=1
 		brew install go
 		brew install helm
 		brew install kubernetes-cli
@@ -102,6 +103,32 @@ install_homebrew() {
 
 		/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+		export HOMEBREW_NO_INSTALL_CLEANUP=1
+		BREW_PREFIX=$(brew --prefix)
+
+		brew install coreutils
+		brew install moreutils
+		brew install findutils
+		brew install gnu-getopt
+		brew install make
+		brew install gnu-sed
+		brew install wget
+		brew install grep
+		brew install screen
+		brew install openssh
+		
+		# Install font tools.
+		brew tap bramstein/webfonttools
+		brew install sfnt2woff
+		brew install sfnt2woff-zopfli
+		brew install woff2
+
+		brew install nmap
+		brew install socat
+		brew install git
+		brew install git-lfs
+		brew install ssh-copy-id
+
 		brew install go
 		brew install helm
 		brew install kubernetes-cli
@@ -114,8 +141,45 @@ install_homebrew() {
 		brew install tmuxinator
 		brew install shellcheck
 		brew install bash
-		brew install bash-completion
+		brew install bash-completion@2
+		brew install docker
+		brew install podman
+
+		if [[ ! -d "${HOME}/.tmux" ]]; then
+			git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+		fi
+
 		brew install --cask alacritty
+		brew install --cask google-chrome
+		brew install --cask microsoft-teams
+		brew install --cask skype
+		brew install --cask whatsapp
+		brew install --cask 1password
+		brew install --cask alfred
+		brew install --cask iterm2
+		brew install --cask appcleaner
+		brew install --cask wireshark
+		brew install --cask utm
+		brew install --cask tower
+		brew install --cask send-to-kindle
+		brew install --cask kindle
+		brew install --cask visual-studio-code
+		brew install --cask xquart
+		brew install --cask kaleidoscope
+		brew install --cask drawio
+		brew install --cask keycastr
+		brew install --cask marked
+		brew install --cask telegram-desktop
+		brew install --cask signal
+
+		# Switch to using brew-installed bash as default shell
+		if ! grep -F -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
+  			echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
+  			chsh -s "${BREW_PREFIX}/bin/bash";
+		fi;
+		
+		# Remove outdated versions from the cellar.
+		brew cleanup
 		;;
 	*)
 		echo "Unknow $PLATFORM, do nothing"
@@ -143,6 +207,7 @@ install_base() {
 			git \
 			build-essential \
 			fonts-powerline \
+			vim \
 			cpu-checker \
 			--no-install-recommends
 
@@ -171,6 +236,7 @@ install_vagrant() {
 			echo -e "\\nHomebrew is not installed\\n"
 			exit 1
 		fi
+		export HOMEBREW_NO_INSTALL_CLEANUP=1
 		brew install hashicorp/tap/hashicorp-vagrant
 		;;
 	esac
@@ -220,6 +286,10 @@ get_dotfiles() {
 }
 
 use_yubikey() {
+	if [[ ! -d "$HOME/.gnupg" ]]; then
+		echo -e "\\n.gnupg directory is not configured, please install dotfiles before configure yubikey"
+		exit 1
+	fi
 	case $PLATFORM in
 	Linux)
 		sudo apt update || true
@@ -244,15 +314,131 @@ use_yubikey() {
 		pip3 install PyOpenSSL
 		pip3 install yubikey-manager
 		sudo systemctl enable --now pcscd
+		ln -sfn "$HOME"/.gnupg/gpg-agent.conf.Linux "$HOME"/.gnupg/gpg-agent.conf
 		;;
 	Darwin)
 		if ! command -v brew 1>/dev/null; then
 			echo -e "\\nHomebrew is not installed\\n"
 			exit 1
 		fi
+		export HOMEBREW_NO_INSTALL_CLEANUP=1
 		brew install gnupg yubikey-personalization hopenpgp-tools ykman pinentry-mac wget pidof
+		brew cleanup
+		ln -sfn "$HOME"/.gnupg/gpg-agent.conf.Darwin "$HOME"/.gnupg/gpg-agent.conf
 		;;
 	esac
+}
+
+cleanupall() {
+	case $PLATFORM in
+	Linux)
+		echo -e "\\nNothing to do for Linux\\n"
+		;;
+	Darwin)
+		echo -e "\\nRemove all symbolic links"
+		find "$HOME"/dotfiles -type f -name ".*" -not -name ".gitignore" -not -name ".git" -not -name ".config" -not -name ".github" -not -name ".*.swp" -not -name ".gnupg" | while read -r file
+		do
+			f=$(basename "$file")
+			rm -f "$HOME"/"$f"
+		done
+		find "$HOME"/dotfiles/bin -type f -not -name "*-backlight" -not -name ".*.swp" | while read -r file
+		do
+			f=$(basename "$file")
+			rm -f /usr/local/bin/"$f"
+		done
+		echo -e "\\nRemove gnupg config directory"
+		rm -rf "$HOME"/.gnupg
+		brew uninstall --cask --force alacritty
+		brew uninstall --cask --force google-chrome
+		brew uninstall --cask --force microsoft-teams
+		brew uninstall --cask --force skype
+		brew uninstall --cask --force whatsapp
+		brew uninstall --cask --force 1password
+		brew uninstall --cask --force alfred
+		brew uninstall --cask --force iterm2
+		brew uninstall --cask --force appcleaner
+		brew uninstall --cask --force wireshark
+		brew uninstall --cask --force utm
+		brew uninstall --cask --force tower
+		brew uninstall --cask --force send-to-kindle
+		brew uninstall --cask --force kindle
+		brew uninstall --cask --force visual-studio-code
+		brew uninstall --cask --force xquart
+		brew uninstall --cask --force kaleidoscope
+		brew uninstall --cask --force drawio
+		brew uninstall --cask --force keycastr
+		brew uninstall --cask --force marked
+		brew uninstall --cask --force telegram-desktop
+		brew uninstall --cask --force signal
+		BREW_PREFIX=$(brew --prefix)
+		if grep -F -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
+  			chsh -s "/bin/zsh";
+			echo -e "\\n--------------------------------------------------------------------------------"
+			echo -e "\\nRemember to delete custom bash configuration from /etc/shells"
+			echo -e "\\nsudo vi /etc/shells"
+			echo -e "\\n--------------------------------------------------------------------------------"
+		fi;
+		echo -e "\\n--------------------------------------------------------------------------------"
+		echo -e "\\nRemember to delete dotfiles directory, can you do it with the following command:"
+		echo -e "\\nrm -rf $HOME/dotfiles"
+		echo -e "\\n--------------------------------------------------------------------------------"
+		if [[ -d "${HOME}/.tmux" ]]; then
+			rm -rf "${HOME}/.tmux"
+		fi
+		if [[ -d "${HOME}/.vscode" ]]; then
+			rm -rf "${HOME}/.vscode"
+		fi
+		if [[ -f "/usr/local/bin/speedtest" ]]; then
+			rm -f "/usr/local/bin/speedtest"
+		fi
+		if [[ -f "$HOME/.gitignore" ]]; then
+			rm -f "$HOME/.gitignore"
+		fi
+		if [[ -f "/usr/local/bin/icdiff" ]]; then
+			rm -f "/usr/local/bin/icdiff"
+		fi
+		if [[ -f "/usr/local/bin/git-icdiff" ]]; then
+			rm -f "/usr/local/bin/git-icdiff"
+		fi
+		if [[ -f "/usr/local/bin/lolcat" ]]; then
+			rm -f "/usr/local/bin/lolcat"
+		fi
+		# create subshell
+		(
+		echo -e "\\nRemove HomeBrew"
+		/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)"
+		)
+		;;
+	esac
+}
+
+qemu() {
+	case $PLATFORM in
+	Linux)
+		echo -e "\\nNothing to do for Linux\\n"
+		;;
+	Darwin)
+		if ! command -v brew 1>/dev/null; then
+			echo -e "\\nHomebrew is not installed\\n"
+			exit 1
+		fi
+		export HOMEBREW_NO_INSTALL_CLEANUP=1
+		brew install qemu
+		brew install lima
+		brew cleanup
+		;;
+	esac
+}
+
+install_scripts() {
+	curl -sSL https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py  > /usr/local/bin/speedtest
+	chmod +x /usr/local/bin/speedtest
+	curl -sSL https://raw.githubusercontent.com/jeffkaufman/icdiff/master/icdiff > /usr/local/bin/icdiff
+	curl -sSL https://raw.githubusercontent.com/jeffkaufman/icdiff/master/git-icdiff > /usr/local/bin/git-icdiff
+	chmod +x /usr/local/bin/icdiff
+	chmod +x /usr/local/bin/git-icdiff
+	curl -sSL https://raw.githubusercontent.com/tehmaze/lolcat/master/lolcat > /usr/local/bin/lolcat
+	chmod +x /usr/local/bin/lolcat
 }
 
 usage() {
@@ -296,11 +482,13 @@ main() {
 	elif [[ $cmd == "yubikey" ]]; then
 		use_yubikey
 	elif [[ $cmd == "qemu" ]]; then
-		echo "Not implemented"
+		qemu
 	elif [[ $cmd == "dotfiles" ]]; then
 		get_dotfiles
 	elif [[ $cmd == "scripts" ]]; then
-		echo "Not implemented"
+		install_scripts
+	elif [[ $cmd == "cleanupall" ]]; then
+		cleanupall
 	else
 		usage
 	fi
